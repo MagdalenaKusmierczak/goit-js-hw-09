5d3ca2f3c86c3d0033215bd952d62d357cb1be96
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2023-03-12 20:09:17 +0100

    base

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+// After clicking «Start», once a secound changes background color on random color.
 
+// After clicking «Stop», background color stop changing.
+
+// Button «Start» is disabled, when color changing is activated.
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null;
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+function onStart() {
+  timerId = setInterval(() => {
+    console.log(`I love async JS!  ${Math.random()}`);
+  }, 1000);
+}
+function onStop() {
+  clearInterval(timerId);
+}
