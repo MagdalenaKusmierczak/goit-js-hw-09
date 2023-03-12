64dbe5e1a857d9172b9b0f434eadd2ba61be3626
MagdalenaKusmierczak <113198413+MagdalenaKusmierczak@users.noreply.github.com>
2023-03-12 19:09:41 +0000

    Deploying to gh-pages from @ MagdalenaKusmierczak/goit-js-hw-09@5d3ca2f3c86c3d0033215bd952d62d357cb1be96 🚀

---
+++ 01-color-switcher.072d868b.js
@@ -0,0 +1,2 @@
+let e=null;document.querySelector("body");const t=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");t.addEventListener("click",(function(){e=setInterval((()=>{console.log(`I love async JS!  ${Math.random()}`)}),1e3)})),n.addEventListener("click",(function(){clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.072d868b.js.map
---
+++ 01-color-switcher.9d990788.js
@@ -0,0 +1,2 @@
+!function(){var e=null,t=(document.querySelector("body"),document.querySelector("[data-start]")),n=document.querySelector("[data-stop]");t.addEventListener("click",(function(){e=setInterval((function(){console.log("I love async JS!  ".concat(Math.random()))}),1e3)})),n.addEventListener("click",(function(){clearInterval(e)}))}();
+//# sourceMappingURL=01-color-switcher.9d990788.js.map
